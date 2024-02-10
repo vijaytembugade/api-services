842ce6a6b24aa80d0925c16b9b43d515f2edf3c6
vijaytembugade <vijaytembugade21@gmail.com>
2024-02-11 01:35:22 +0530

    feat: added prettier and config with other eslint config

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
-import express from 'express'
-import type { Express } from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import type { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-const app: Express = express()
+const app: Express = express();
 
-dotenv.config()
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Apis are running' })
-})
-app.get('/test', (req, res) => {
-  res.status(200).json({ message: 'Apis are running- test' })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Apis are running" });
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "Apis are running- test" });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log('app is running on PORT', process.env.PORT)
-})
+  console.log("app is running on PORT", process.env.PORT);
+});
