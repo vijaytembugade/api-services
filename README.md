5ed2a99f66ef4b8b33a3a1fae795a082ee8de92c
vijaytembugade <vijaytembugade21@gmail.com>
2024-03-03 16:15:34 +0530

    chore: updated readme

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ This is backend project have multiples api services.
 This project is build with NodeJs, ExpressJs, MongoDB, and some the packages.
 Project is build with typescript.
 
-## Local setup
+Update 1.0 :
+Added the validation for the request using `express-request-validator`
+
+Hosted url : https://api-services-3td0.onrender.com
+
+## Set up on Local setup
 
 1. clone the repo
 2. move inside `api-services` directory
--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const test_routes_1 = __importDefault(require("./routes/test.routes"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json({ limit: '16kb' }));
+app.use((0, cors_1.default)({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express_1.default.urlencoded({ extended: true, limit: '8kb' }));
+app.use(express_1.default.static('public'));
+app.use((0, cookie_parser_1.default)());
+// routes
 app.use('/', test_routes_1.default);
 app.use('/api/v1/user', user_routes_1.default);
 exports.default = app;
