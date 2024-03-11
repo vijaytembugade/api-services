3ed4885ad48b6f0e1094c6d366f34f88fb3a6f6f
vijaytembugade <vijaytembugade21@gmail.com>
2024-03-11 23:58:23 +0530

    redis client and service

---
+++ src/service/redis.ts
@@ -0,0 +1,16 @@
+import { colorInfo } from '@utils/colorCli';
+import { createClient } from 'redis';
+import type { RedisClientType } from 'redis';
+
+let redisClient: RedisClientType;
+
+const connectRedis = async () => {
+    redisClient = createClient({
+        url: process.env.REDIS_CLIENT_URL,
+    });
+    redisClient.on('error', (err) => console.log('Redis Client Error', err));
+    await redisClient.connect();
+    console.log(colorInfo('Redis is also connected'));
+};
+
+export { connectRedis, redisClient };
