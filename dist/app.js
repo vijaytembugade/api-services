468ba4fa4b907ffdf2312cf1682e15b18cdac81d
vijaytembugade <vijaytembugade21@gmail.com>
2024-03-14 01:47:39 +0530

    dist

--- dist/app.js
+++ dist/app.js
@@ -3,21 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const user_routes_1 = __importDefault(require("./routes/user.routes"));
+const tokenValidator_1 = __importDefault(require("./middlewares/tokenValidator"));
 const test_routes_1 = __importDefault(require("./routes/test.routes"));
+const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json({ limit: '16kb' }));
 app.use((0, cors_1.default)({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
-app.use(express_1.default.urlencoded({ extended: true, limit: '8kb' }));
+app.use(express_1.default.json({ limit: '16kb' }));
+app.use(express_1.default.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express_1.default.static('public'));
 app.use((0, cookie_parser_1.default)());
 // routes
-app.use('/', test_routes_1.default);
 app.use('/api/v1/user', user_routes_1.default);
+app.use(tokenValidator_1.default);
+app.use('/', test_routes_1.default);
 exports.default = app;
